bd24d5336f2ec9722dac9ffbf8fa22abef377f57
xerexcoded <arnavbanerjee61@gmail.com>
2021-10-19 02:49:15 +0530

    this is exciting zzz

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::io::prelude::*;
 
 #[derive(Debug)]
+pub enum QuoteStates {
+    Left,
+    Right,
+    Ignore,
+}
+pub struct CollectQuotes {
+    pub buffer: String,
+    pub saved: Vec<String>,
+    pub state: QuoteStates,
+}
 fn main() {
     println!("Hello, world!");
 }
